f2aacb16be6acd248bee97d328c5d2f926bef9bc
Victor Wang <victor.wang@me.com>
2017-09-08 12:08:10 -0700

    complete

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+  onHover(event) {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseEnter={this.onHover.bind(this)}
+                        onMouseLeave={this.onHover.bind(this)}>
+        {this.props.item}
+      </li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.list.map(item => <GroceryListItem item={item}/>)}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList list={['bananas', 'watermelon', 'orange']}/>, document.getElementById('app'));
